864819f5abd366f1b656a1aff881a3294c1c4ec5
haric95 <hari-c@hotmail.co.uk>
2021-03-26 18:41:29 +0000

    Set up page and components directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Landing } from "./pages/landing";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Landing />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/landing.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Landing: React.FC = () => {
+  return <h1>Landing</h1>;
+};
