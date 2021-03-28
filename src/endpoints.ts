6ab7cd536ae3c25242bd27c4e1906bbaaf80c27b
Hari Chauhan <hari-c@hotmail.co.uk>
2021-03-28 21:14:11 +0100

    update baseurl

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,8 +1,8 @@
-const BASE = "http://ec2-54-247-19-173.eu-west-1.compute.amazonaws.com:8000";
+const BASE = "https://lexiii.co.uk";
 
 export const endpoints = {
   partners: `${BASE}/search`,
-  sign_in: `${BASE}/sign-in`,
+  sign_in: `${BASE}/signin`,
   sign_up: {
     migrant: `${BASE}/signup/migrant`,
     volunteer: `${BASE}/signup/volunteer`,
